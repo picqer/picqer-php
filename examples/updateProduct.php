dc05aed124b6364ef9481ccad056a5a8ae50c4aa
Rico Hageman <info@ricohageman.nl>
2017-02-07 12:57:27 +0100

    Created update product method and added an example

---
+++ examples/updateProduct.php
@@ -0,0 +1,19 @@
+<?php
+
+require('../src/Client.php');
+
+// Start 
+$apiclient = new Picqer\Api\Client('--clientsubdomain--', '--apikey--');
+
+// Retrieve the previously inserted product
+$product = $apiclient->getProductsByProductcode('DKS-092383');
+
+// Compose the put data
+// Note: if one would like to change a product field
+//			This can be done with the following syntax:
+//			$data = array('productfields' => array(array('idproductfields' => 1, 'value' => 1), array('idproductfields' => 2, 'value' => 2)))
+$data = array(
+	'name' 			=>	'Apple iPod Shuffle Green')
+);
+
+var_dump($apiclient->updateProduct($product['data']['idproduct'], $data));
